955548a2bb75eeaf08ac4239606214ec58337898
kareembh7 <kareem.banihani@hotmail.com>
2024-08-08 15:55:38 +0300

    removed script and added seperate js files for each category

---
+++ news.js
@@ -0,0 +1,40 @@
+const apiURL = 'https://newsapi.org/v2/everything';
+const apiKey = '7b8e680d0cb1458aa0e9af59512e2a32';
+const query = 'automotive';
+
+async function fetchNews() {
+  try {
+    const response = await fetch(`${apiURL}?q=${query}&apiKey=${apiKey}`);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok' + response.statusText);
+    }
+
+    const data = await response.json();
+
+    displayNews(data.articles);
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+}
+
+function displayNews(articles) {
+  const newsContainer = document.getElementById('news-container');
+  newsContainer.innerHTML = '';
+
+  articles.forEach(article => {
+    const articleElement = document.createElement('div');
+    articleElement.className = 'article';
+
+    articleElement.innerHTML = `
+      <h2><a href="${article.url}" target="_blank">${article.title}</a></h2>
+      <p>${article.description}</p>
+    `;
+
+    newsContainer.appendChild(articleElement);
+  });
+}
+
+fetchNews();
+
+setInterval(fetchNews, 1800000);
